acb85642cff46ad547ae9a415724ccf45f8ef1e8
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-10-23 09:22:09 +0200

    My First Commit

---
+++ src/GitHubDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Date: 2020-10-23
+ * Time: 09:09
+ * Project: Lektion16
+ * Copyright: MIT
+ */
+public class GitHubDemo {
+  public static void main(String[] args) {
+    System.out.println("Hello GitHub");
+  }
+}
