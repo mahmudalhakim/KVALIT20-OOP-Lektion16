5616311aa083b47735f102e36235e6f0c377d2ee
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-10-23 14:01:56 +0200

    Att beräkna mittpunkten och lite utskrifter

--- src/Point.java
+++ src/Point.java
@@ -36,4 +36,31 @@ public class Point {
     return Math.sqrt(Math.pow(p2.x - p1.x, 2) + (p2.y - p1.y) * (p2.y - p1.y));
   }
 
+
+  /**
+   * En klassmetod som beräknar mittpunkten
+   *
+   * @param p1 är en referens till den första punkten
+   * @param p2 är en referens till den andra punkten
+   * @return en referens till mittpunkten
+   */
+  public static Point midPoint(Point p1, Point p2) {
+
+    Point p3 = new Point();
+    p3.x = (p1.x + p2.x) / 2;
+    p3.y = (p1.y + p2.y) / 2;
+
+    return p3;
+
+  }
+
+  /**
+   * En klassmetod som skriver ut en punkt
+   * @param p en punkt
+   */
+  public static void print(Point p) {
+    System.out.println("(" + p.x + ", " + p.y + ")");
+  }
+
+
 }
--- src/PointDemo.java
+++ src/PointDemo.java
@@ -17,29 +17,51 @@ public class PointDemo {
     System.out.println("p2 = " + p2);
 
     // Skriv ut punkterna
-    System.out.println("p1(" + p1.x + " , " + p1.y + ")" );
-    System.out.println("p2(" + p2.x + " , " + p2.y + ")" );
+    print(p1, p2);
 
     // Flytta punkt 1 (p1)
     p1.x = 5;   // Flytta 5 steg till höger
     p1.y = -5;  // Flytta 5 steg neråt
 
     // Beräkna avståndet mellan p1 och p2
-    double d = Point.distance(p1,p2);
-    System.out.printf("Avstånd = %.2f \n" , d);
+    double d = Point.distance(p1, p2);
+    System.out.printf("Avstånd = %.2f \n", d);
 
     // Flytta punkt 1
     p1.x = 10;
     p1.y = 0;
 
-    System.out.printf("Avstånd = %.2f \n" , Point.distance(p1,p2));
-    System.out.printf("Avstånd = %.2f \n" , Point.distance(p2,p1));
+    // Skriv ut punkterna
+    print(p1, p2);
+
+    System.out.printf("Avstånd = %.2f \n", Point.distance(p1, p2));
+    System.out.printf("Avstånd = %.2f \n", Point.distance(p2, p1));
 
     // Flytta punkt 1
     p1.x = 3;
     p1.y = 4;
 
-    System.out.printf("Avstånd = %.2f \n" , Point.distance(p1,p2));
+    print(p1, p2);
 
+    System.out.printf("Avstånd = %.2f \n", Point.distance(p1, p2));
+
+
+    // Beräkna mittpunkten
+    Point midpoint = Point.midPoint(p1, p2);
+    System.out.print("Mittpunkten är " );
+    Point.print(midpoint);
+
+
+  }
+
+  /**
+   * En privat hjälpmetod som skriver ut 2 punkter
+   *
+   * @param p1 den första punkten
+   * @param p2 den andra punkten
+   */
+  private static void print(Point p1, Point p2) {
+    System.out.println("p1(" + p1.x + " , " + p1.y + ")");
+    System.out.println("p2(" + p2.x + " , " + p2.y + ")");
   }
 }
