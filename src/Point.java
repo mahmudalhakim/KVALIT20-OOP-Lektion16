c7885e925102aa642e63cf50e0b3e2f51c79f5d9
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-10-23 11:40:30 +0200

    Skapa klassen Point

---
+++ src/Point.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Date: 2020-10-23
+ * Time: 11:38
+ * Project: Lektion16
+ * Copyright: MIT
+ *
+ * Point är en klass som beskriver
+ * en punkt i 2D koordinatsystem.
+ */
+public class Point {
+
+  // Instansvariabler
+  double x;  // x-koordinat
+  double y;  // y-koordinat
+
+}
