e527776eb180bba49dbf3c791f958afba14f39f1
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2020-10-23 09:54:26 +0200

    Skapat klassen Car

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Date: 2020-10-23
+ * Time: 09:46
+ * Project: Lektion16
+ * Copyright: MIT
+ */
+public class Car {
+  int year;
+  String model;
+  int price;    // 9.50 kr = 950 öre (Diskussion)
+}
